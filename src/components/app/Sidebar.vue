59611d8c3f25f0a0da6b6a224b3485b5740c3dcc
vvalunskiy <vvalunskii@c-i-p.ru>
2019-11-29 19:04:28 +0300

    main layout and routing

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul class="sidenav app-sidenav open">
+
+        <router-link
+            v-for="link in links"
+            :key="link.url"
+            tag="li"
+            active-class="active"
+            :to="link.url"
+            :exact="link.exact"
+        >
+            <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+        </router-link>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            links: [
+                {title: 'Счет', url: '/', exact: true}, //тк данный url есть в составе всех других, то этот пункт постоянно подсвечивается. exact говорит что url должен быть именно таким а не содержаться к часть урла
+                {title: 'История', url: '/history'},
+                {title: 'Планирование', url: '/planning'},
+                {title: 'Новая запись', url: '/record'},
+                {title: 'Категории', url: '/categories'}
+            ]
+        })
+    }
+
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="app-main-layout">
+        <Navbar />
+        <Sidebar />
+        <main class="app-content">
+            <div class="app-page">
+                <router-view />
+            </div>
+        </main>
+
+        <div class="fixed-action-btn">
+            <a class="btn-floating btn-large blue" href="#">
+                <i class="large material-icons">add</i>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Navbar from '@/components/app/Navbar.vue'
+    import Sidebar from '@/components/app/Sidebar.vue'
+
+    export default {
+        name: 'main-layout',
+        components: {
+            Navbar, Sidebar
+        }
+    }
+</script>
+
