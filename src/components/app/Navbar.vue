e9d67c6df6e7a03da9fa902155e0a0403742405f
vvalunskiy <vvalunskii@c-i-p.ru>
2019-11-29 20:33:25 +0300

    make user dropdown menu work

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -14,6 +14,7 @@
                             class="dropdown-trigger black-text"
                             href="#"
                             data-target="dropdown"
+                            ref="dropdown"
                     >
                         USER NAME
                         <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
 
                     <ul id='dropdown' class='dropdown-content'>
                         <li>
-                            <a href="/profile" class="black-text">
+                            <router-link to="/profile" class="black-text">
                                 <i class="material-icons">account_circle</i>Профиль
-                            </a>
+                            </router-link>
                         </li>
                         <li class="divider" tabindex="-1"></li>
                         <li>
-                            <a href="/login" class="black-text">
+                            <a href="#" class="black-text" @click.prevent="logout">
                                 <i class="material-icons">assignment_return</i>Выйти
                             </a>
                         </li>
@@ -37,3 +38,21 @@
         </div>
     </nav>
 </template>
+
+<script>
+    export default {
+        mounted() {
+            M.Dropdown.init(this.$refs.dropdown, {
+                constrainWidth: true,
+                coverTrigger: false
+            })
+        },
+        methods: {
+            logout() {
+                console.log('Logout')
+                this.$router.push('/login?message=logout')
+            }
+        }
+    }
+
+</script>
